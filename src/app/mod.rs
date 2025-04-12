79803edc93fcb90e0051600da6b641fdc102618a
moonyango <mosesadrian825@gmail.com>
2025-04-12 16:30:52 +0300

    feat: eventhandler implementation for appstate. Handle events like update, draw, mouse click events and keydown events for escape and return

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -1,11 +1,11 @@
-pub mod events; // handle events (mouse and keyboard inputs)
-pub mod drawing; // handle drawing on the canvas
+pub mod drawing;
+pub mod events; // handle events (mouse and keyboard inputs) // handle drawing on the canvas
 
-use ggez::{Context, GameResult};
 use ggez::event::EventHandler;
+use ggez::{Context, GameResult};
 
 pub struct AppState {
-    pub control_points : Vec<(f32,f32)>,
+    pub control_points: Vec<(f32, f32)>,
     pub animating: bool,
 }
 
@@ -14,6 +14,48 @@ impl AppState {
         Self {
             control_points: Vec::new(),
             animating: false,
+        }
+    }
+}
+
+impl EventHandler for AppState {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult {
+        // for now no update logic.
+        Ok(())
+    }
+    fn draw(&mut self, _ctx: &mut Context) -> GameResult {
+        drawing::draw_canvas(ctx, &self.control_points)
+    }
+    fn mouse_button_down_event(
+        &mut self,
+        _ctx: &mut Context,
+        _button: ggez::event::MouseButton,
+        _x: f32,
+        _y: f32,
+    ) {
+        if button == ggez::event::MouseButton::Left {
+            events::handle_mouse_click(self, x, y)
+        }
+    }
+    fn key_down_event(
+        &mut self,
+        ctx: &mut Context,
+        keycode: Option<ggez::event::KeyCode>,
+        _mods: ggez::input::keyboard::KeyMods,
+        _repeat: bool,
+    ) {
+        if some(key) = keycode {
+            match key {
+                ggez::event::KeyCode::Return => {
+                    if self.control_points.len() >= 2 {
+                        self.animating = true
+                    } else {
+                        println!("Please add at least 2 control points first.");
+                    }
+                }
+                ggez::event::KeyCode::Escape => ggez::event::quit(ctx),
+                _ => {}
+            }
         }
     }
 }
