85b3d0205a1b71f0a2aeface3d26d3ce7d3c9102
moonyango <mosesadrian825@gmail.com>
2025-04-12 16:56:33 +0300

    feat: build context and run the game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+mod app;
+
+use ggez::event;
+use ggez::winit::event_loop;
+use ggez::{Context, ContextBuilder, GameResult};
+
+fn main() -> GameResult {
+    let (mut ctx, mut event_loop) = ContextBuilder::new("CHAIKIN'S ALGORITHM", "MOSES")
+        .build()
+        .expect("Failed to build context");
+    let state = app::AppState::new(&mut ctx);
+    event::run(ctx, event_loop, state)
 }
